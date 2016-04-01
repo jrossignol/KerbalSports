198ca587534b3ee7768b4f1e9d89f7eace5b1770
Jeremie Rossignol <jrossignol@gmail.com>
2016-04-01 15:51:31 -0600

    Initial full release.

--- source/KerbalSports/Properties/AssemblyInfo.cs
+++ source/KerbalSports/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KerbalFishingProgram")]
+[assembly: AssemblyTitle("KerbalSports")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KerbalFishingProgram")]
+[assembly: AssemblyProduct("KerbalSports")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("0.0.2")]
-[assembly: AssemblyInformationalVersion("0.0.2")]
-[assembly: KSPAssembly("KerbalFishingProgram", 1, 0)]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: KSPAssembly("KerbalSports", 1, 0)]
